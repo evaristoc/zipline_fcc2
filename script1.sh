c70cdd8796b735c9c7fd2396d78d4e5bfcb0484c
ec <varikvi@yahoo.com>
2015-11-03 17:24:35 +0100

    Initial Commit

--- script1.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-export PATH=/home/ec/Public/node_modules/.node/bin:/usr/local/heroku/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/usr/local/games
-mongod --dbpath=/home/ec/Public/freecodecamp/basejumps/mongodb/data/db
